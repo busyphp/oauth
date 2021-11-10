4288c28e85d02f6cf31de218b9c5763a2235f079
刘晓彬 <563859671@qq.com>
2021-11-10 22:30:30 +0800

    升级 busyphp6

---
+++ src/app/controller/ManagerController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace BusyPHP\oauth\app\controller;
+
+use BusyPHP\contract\abstracts\PluginManager;
+use Exception;
+use think\Response;
+
+/**
+ * 插件管理
+ * @author busy^life <busy.life@qq.com>
+ * @copyright (c) 2015--2021 ShanXi Han Tuo Technology Co.,Ltd. All rights reserved.
+ * @version $Id: 2021/11/10 下午10:24 ManagerController.php $
+ */
+class ManagerController extends PluginManager
+{
+    /**
+     * 返回模板路径
+     * @return string
+     */
+    protected function viewPath() : string
+    {
+        return __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR;
+    }
+    
+    
+    /**
+     * 安装插件
+     * @return Response
+     * @throws Exception
+     */
+    public function install() : Response
+    {
+    }
+    
+    
+    /**
+     * 卸载插件
+     * @return Response
+     * @return Exception
+     */
+    public function uninstall() : Response
+    {
+    }
+    
+    
+    /**
+     * 设置插件
+     * @return Response
+     * @return Exception
+     */
+    public function setting() : Response
+    {
+        return response();
+    }
+}
---
+++ src/model/MemberOauthInfo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BusyPHP\oauth\model;
+
+/**
+ * OAuth用户信息结构
+ * @author busy^life <busy.life@qq.com>
+ * @copyright (c) 2015--2021 ShanXi Han Tuo Technology Co.,Ltd. All rights reserved.
+ * @version $Id: 2021/11/10 下午10:20 MemberOauthInfo.php $
+ */
+class MemberOauthInfo extends MemberOauthField
+{
+}
